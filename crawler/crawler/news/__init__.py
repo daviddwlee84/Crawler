08e258a77f944e89c253c8681267a3e3b74446c0
David Lee <daviddwlee84@gmail.com>
2020-08-28 10:21:03 +0800

    Add __all__

--- crawler/crawler/news/__init__.py
+++ crawler/crawler/news/__init__.py
@@ -3,6 +3,17 @@ import sys
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(curr_dir))
 
+# https://stackoverflow.com/questions/1944569/how-do-i-write-good-correct-package-init-py-files
+
+__all__ = [
+    'CCTVNewsCrawler',
+    'NetEaseNewsCrawler',
+    'SinaNewsCrawler',
+    'SohuNewsCrawler',
+    'TencentNewsCrawler',
+    'TouTiaoNewsCrawler'
+]
+
 from .cctv import CCTVNewsCrawler
 from .netease import NetEaseNewsCrawler
 from .sina import SinaNewsCrawler
